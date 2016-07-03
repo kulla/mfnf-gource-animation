ebb4334846a895d862374631857df28b7669ffb2
Stephan Kulla <git.mail@kulla.me>
2016-07-03 19:46:12 +0200

    add copyright notice to source files

--- create_mfnf_git.py
+++ create_mfnf_git.py
@@ -1,3 +1,15 @@
+# create_mfnf_git.py - script for saving the history of MFNF in a git repo
+#
+# Written in 2016 by Stephan Kulla ( http://kulla.me )
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 import os
 import requests
 import re
